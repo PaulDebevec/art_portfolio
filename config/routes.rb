47e23e9d5346ce7c3c66ab3396b9f45ba5a1a523
Paul Debevec <paulmdebevec@gmail.com>
2020-02-18 09:59:59 -0700

    Add routes for new portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/portfolios', to: 'portfolios#index'
+  get '/portfolios/new', to: 'portfolios#new'
+  post '/portfolios', to: 'portfolios#create'
   get '/portfolios/:portfolio_id', to: 'portfolios#show'
 end
