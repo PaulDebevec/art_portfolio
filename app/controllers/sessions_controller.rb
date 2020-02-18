53ff3d082df894aee7cc73c4b6e8e8b3563b2295
Paul Debevec <paulmdebevec@gmail.com>
2020-02-18 14:05:24 -0700

    Add functionality for user login and log out as well as pw varification

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.username}!"
+      redirect_to '/dashboard'
+    else
+      flash[:error] = "Sorry, your credentials are incorrect."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get '/', to: 'welcome#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/dashboard', to: 'dashboard#index'
+
   get '/portfolios', to: 'portfolios#index'
   get '/portfolios/new', to: 'portfolios#new'
   post '/portfolios', to: 'portfolios#create'
