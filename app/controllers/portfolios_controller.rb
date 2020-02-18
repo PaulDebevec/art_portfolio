27e20087318ccd7cec3424a514f2e3d21f7f6cd8
Paul Debevec <paulmdebevec@gmail.com>
2020-02-18 10:00:54 -0700

    Add functionality to controller for new portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,4 +6,23 @@ class PortfoliosController < ApplicationController
   def show
     @portfolio = Portfolio.find(params[:portfolio_id])
   end
+
+  def new
+  end
+
+  def create
+    portfolio = Portfolio.new(portfolio_params)
+    if portfolio.save
+      flash[:success] = "#{portfolio.name} Has Been Created"
+      redirect_to '/portfolios'
+    else
+      flash[:error] = portfolio.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+  private
+
+  def portfolio_params
+    params.permit(:name, :description)
+  end
 end
