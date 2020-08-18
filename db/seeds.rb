f9331258c33c079ab280aae50911562f2c63ac7a
Paul Debevec <paulmdebevec@gmail.com>
2020-08-18 11:07:16 -0600

    Add temp test admin user to seeds for development testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@ Portfolio.destroy_all
 
 Portfolio.create(name: "Traditional")
 Portfolio.create(name: "Digital", description: "This portfolio consists of digital artwork")
+User.create(username: "admin", password: "password", role: 1)
